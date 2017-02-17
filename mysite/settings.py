a6a9c01e46267425ec30a7daae84773d81846039
luckybirdme <luckybirdme@users.noreply.github.com>
2017-02-17 19:33:48 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'root',
-        'PASSWORD': 'chen2016',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
